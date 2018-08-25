9c2545a4c1f2631b9c6459a19647d760cf3f4fe3
ling <1007890788@qq.com>
2018-08-25 21:53:31 +0800

    finished lesson27

--- lesson27/md5.js
+++ lesson27/md5.js
@@ -2,4 +2,5 @@ const common = require("./libs/common");
 
 var str1 = "amxjsngs"
 var str = common.md5(str1+'ssksssssssss6yv');
+// 得到结果
 console.log(str);
--- lesson27/route/admin.js
+++ lesson27/route/admin.js
@@ -1,16 +1,19 @@
 const express = require("express");
+const common = require("../libs/common");
 module.exports = function(){
     var router = express.Router();
 
     //检查登录状态
     router.use((res,req,next)=>{
-        if(!req.session["admin_id"]&&req.url!='/login'){
+        if(!req.session['admin_id']&&req.url!='/login'){
             res.redirect('admin/login');//重定向返回登录
         }else{
             next();
         }
     })
-    router.get('/login',(req,res)=>{
+
+    router.use('/login',(req,res)=>{
+        console.log(req.body);
         res.render('admin/login.ejs',{});
     })
     // router.get("/",(req,res)=>{
